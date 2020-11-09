dc4ebc863bb2de66c451acbf3061846ed5a093b2
Garren Souza <contato.garren@gmail.com>
2020-11-09 14:42:57 -0300

    sweep sweep nothing to see here!

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "./dataStructures/triePatricia.h"
+#include "./dataStructures/triePatricia/triePatricia.h"
 
 int main() {
     // char *string = (char *)malloc(4 * sizeof(char));
